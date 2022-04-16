c04ad72a7f28f0d254646df7ade37d151d3c92c8
Jobair Shakib <shakib.jobair@gmail.com>
2022-04-16 23:31:37 +0600

    fake data added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
-import Home from './Pages/Home/Services/Home/Home';
+import Home from './Pages/Home/Home/Home';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 
